760fa23f4c11ee81fac4f79fabab8d253f33e8b6
GakuruUri <urigakuru@icloud.com>
2024-07-23 21:38:12 +0300

    Check and rectify code

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -2,7 +2,7 @@ import math
 import random
 
 
-class Player:
+class Player():
     def __init__(self, letter):
         # letter is x or o
         self.letter = letter
@@ -30,52 +30,15 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. input move (0-9):')
+            square = input(self.letter + '\'s turn. input move (0-8):')
             # we're going to check that this is a correct value by trying to cast.
             # it to an integer, and if it's not, then we say its valid
             # if that spot is not available on the board, we also say its invalid
             try:
                 val = int(square)
-                if val not in game.available_moves:
+                if val not in game.available_moves():
                     raise ValueError
-                valid_square = True # if these are successful, then yay!
+                valid_square = True  # if these are successful, then yay!
             except ValueError:
                 print('Invalid square. Try again.')
         return val
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
