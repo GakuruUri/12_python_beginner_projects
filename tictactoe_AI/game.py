b65f79ccd3af6f965bb2b6be143a7b2419467f0d
GakuruUri <urigakuru@icloud.com>
2024-07-28 07:37:34 +0300

    Just did the minimax algo aaaaand what was that?

--- tictactoe_AI/game.py
+++ tictactoe_AI/game.py
@@ -1,33 +1,51 @@
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 
+
+# Minimax algorithm
 
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)]
-        self.current_winner = None
+        self.board = [
+            " " for _ in range(9)
+        ]  # We will use a single list to represent a 3*3 board
+        self.current_winner = None  # Keep track of the winner
 
     def print_board(self):
+        # This is just getting the rows
         for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
+            print("| " + " | ".join(row) + " |")
 
     @staticmethod
     def print_board_nums():
+        # 0 | 1 | 2 etc. (tells us what number corresponds to what box)
         number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
-            print('| ' + ' | '.join(row) + ' |')
+            print("| " + " | ".join(row) + " |")
 
+    # def available_moves(self):
+    #     # return []
+    #     moves = []
+    #     for (i, spot) in enumerate(self.board):
+    #         # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'x')]
+    #         if spot == ' ':
+    #             moves.append(i)
+    #     return moves
+
+    # The above as a list comprehension
     def available_moves(self):
-        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        return [i for i, spot in enumerate(self.board) if spot == " "]
 
     def empty_squares(self):
-        return ' ' in self.board
+        return " " in self.board
 
     def num_empty_squares(self):
-        return self.board.count(' ')
+        return self.board.count(" ")
+        # return len(self.available_moves())
 
     def make_move(self, square, letter):
-        if self.board[square] == ' ':
+        # If valid move, then make the move(assign square to the letters) Then return True, if invalid False
+        if self.board[square] == " ":
             self.board[square] = letter
             if self.winner(square, letter):
                 self.current_winner = letter
@@ -35,23 +53,34 @@ class TicTacToe:
         return False
 
     def winner(self, square, letter):
+        # winner if 3 in a row anywhere. We have to check all of these
+
+        # First check row
         row_ind = square // 3
         row = self.board[row_ind * 3: (row_ind + 1) * 3]
         if all([spot == letter for spot in row]):
             return True
 
+        # Then check column
         col_ind = square % 3
         column = [self.board[col_ind + i * 3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
 
+        # Then check diagonals
+        # But only if the diagonal is an even number(0, 2, 4, 6, 8)
+        # these are the only numbers that can win a diagonal
+
         if square % 2 == 0:
-            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]  # left to right diagonal
             if all([spot == letter for spot in diagonal1]):
                 return True
-            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]  # right to right diagonal
             if all([spot == letter for spot in diagonal2]):
                 return True
+
+        # If all fails
         return False
 
 
@@ -59,33 +88,63 @@ def play(game, x_player, o_player, print_game=True):
     if print_game:
         game.print_board_nums()
 
-    letter = 'x'
-
+    letter = "X"  # Starting letter
+    #   Iterate while the game still has empty squares (we don't have to worry about the winner because we'll just
+    #   return that which breaks the loop)
     while game.empty_squares():
-        if letter == 'O':
+        # Get the move from the appropriate player
+        if letter == "O":
             square = o_player.get_move(game)
         else:
             square = x_player.get_move(game)
 
+        # Let's define a function to make a move.
         if game.make_move(square, letter):
             if print_game:
-                print(letter + f' makes a move to a square {square}')
+                print(letter + f"make a move to square {square}")
                 game.print_board()
-                print('')
+                print("")  # Just an empty line
 
             if game.current_winner:
                 if print_game:
-                    print(letter + ' wins!')
+                    print(letter + "wins!")
                 return letter
 
-            letter = 'O' if letter == 'X' else 'X'
-            time.sleep(0.8)
+                # After we made our move, we need to alternate letters
+            letter = "O" if letter == "X" else "X"  # switches player
+
+            time.sleep(.8)
+
+        # # above is same as
+        # if letter == 'X':
+        #     letter = 'O'
+        # else:
+        #     letter = 'X'
     if print_game:
-        print('It\'s a tie!')
+        print("It's a tie!")
 
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(1000):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+
+    print(f" After 1000 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties.")
+
+# if __name__ == "__main__":
+#     x_player = HumanPlayer("X")
+#     # o_player = RandomComputerPlayer("O")
+#     o_player = GeniusComputerPlayer('O')
+#     t = TicTacToe()
+#     play(t, x_player, o_player, print_game=True)
--- tictactoe_AI/player.py
+++ tictactoe_AI/player.py
@@ -1,11 +1,15 @@
-import math
 import random
+import math
 
+
+# 299,875,899.672
 
 class Player:
     def __init__(self, letter):
+        # Letter is x or o
         self.letter = letter
 
+    # We want all players to get their next move given a name
     def get_move(self, game):
         pass
 
@@ -15,6 +19,7 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
+        # get a random valid spot for our next move
         square = random.choice(game.available_moves())
         return square
 
@@ -27,15 +32,92 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-8):')
+            square = input(self.letter + "\'s turn. Input move(0-8): ")
+            # We are going to check that this is a correct value by trying to cast it to an integer, and if it's not,
+            # then we say it's valid, if the spot isn't available on the board, we also say it's valid.
 
             try:
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square = True
-
+                valid_square = True  # If these are successful, then yay!
             except ValueError:
-                print('Invalid square. Try again')
+                print("Invalid square. Try again!")
         return val
+
+
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())  # randomly choose one
+        else:
+            # Get the square based off the minimax algorithm
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'  # the other player
+
+        # First we want to check if the previous move is a winner
+        # This is our base case
+        if state.current_winner == other_player:
+            # We should return position AND score because we need to keep track of the score
+            # for minimax to work
+            return {'position': None,
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                            state.num_empty_squares() + 1)
+                    }
+        elif not state.empty_squares():  # no empty squares
+            return {'position': None, 'score': 0}
+
+        # Initialize some dictionaries
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # Each score should maximize(should be larger)
+        else:
+            best = {'position': None, 'score': math.inf}    # Each score should minimize
+
+        for possible_move in state.available_moves():
+            # Step 1: make a move, try that spot
+            state.make_move(possible_move, player)
+            # Step 2: recurse using minimax to stimulate a game after making that move
+            sim_score = self.minimax(state, other_player)   # Now, we alternate players
+            # Step 3: undo the move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move   # other wise this will be messed up by the recussion
+
+            # Step 4: update the dictionaries if necessary
+            if player == max_player:    # we're trying to maximize the max_player
+                if sim_score['score'] > best['score']:
+                    best = sim_score    # replace best
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score    # replace best
+
+        return best
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
